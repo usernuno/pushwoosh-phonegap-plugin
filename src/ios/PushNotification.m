284cd2ffe6e0d9cc79af63ee870d0117d1cfdca7
Vitor Oliveira <vmfoliveira89@gmail.com>
2018-07-19 18:34:15 +0100

    Get Background color defined as a preference for color inbox. PS. This feature, currently, is not used on our plugin

--- src/ios/PushNotification.m
+++ src/ios/PushNotification.m
@@ -490,9 +490,20 @@ void pushwoosh_swizzle(Class class, SEL fromChange, SEL toChange, IMP impl, cons
 - (UIColor *)colorFromInboxStyleDict:(NSDictionary *)dict forKey:(NSString *)key {
     NSObject *object = dict[key];
     if (object != nil && [object isKindOfClass:[NSString class]]) {
-        return [((CDVViewController *)self.viewController) colorFromColorString:(NSString *)object];
+        // As colorFromColorString method can't be available on shell, it was replaced for the default background color defined as a preference
+        //return [((CDVViewController *)self.viewController) colorFromColorString:(NSString *)object];
+        NSString *backgroundColor = [self.commandDelegate.settings objectForKey:@"BackgroundColor"];
+        return backgroundColor ? [PushNotification colorFromHexString:backgroundColor] : nil;
     }
     return nil;
+}
+
++ (UIColor *)colorFromHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
 
 - (NSString *)stringFromInboxStyleDict:(NSDictionary *)dict forKey:(NSString *)key {
